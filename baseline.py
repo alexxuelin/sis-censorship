d718818b2f9418af853ef1c7ad02ec3058d36f47
Alex Lin <alexlin@Alexs-MacBook-Pro-53.local>
2018-02-20 23:01:32 -0500

    .py baseline file added

---
+++ baseline.py
@@ -0,0 +1,44 @@
+import numpy as np
+from scipy.integrate import odeint
+import matplotlib.pyplot as plt
+# Initial Conditions
+N = 1000
+I0, R0 = 1, 0
+S0 = N - I0 - R0
+# Contact rate = alpha; mean recovery rate = beta, (in 1/days)
+alpha, beta = 0.2, 1./10
+# A grid of time points (in days)
+t = np.linspace(0, 160, 160)
+
+# The SIR model differential equations.
+def deriv(y, t, N, alpha, beta):
+    S, I, R = y
+    dSdt = -alpha * S * I / N
+    dIdt = alpha * S * I / N - beta * I
+    dRdt = beta * I
+    return dSdt, dIdt, dRdt
+
+# Initial conditions vector
+y0 = S0, I0, R0
+# Integrate the SIR equations over the time grid, t.
+ret = odeint(deriv, y0, t, args=(N, alpha, beta))
+S, I, R = ret.T
+
+# Plot the data on three separate curves for S(t), I(t) and R(t)
+fig = plt.figure(facecolor='w')
+ax = fig.add_subplot(111, axis_bgcolor='#dddddd', axisbelow=True)
+ax.plot(t, S/1000, 'b', alpha=0.5, lw=2, label='Susceptible')
+ax.plot(t, I/1000, 'r', alpha=0.5, lw=2, label='Infected')
+ax.plot(t, R/1000, 'g', alpha=0.5, lw=2, label='Recovered with immunity')
+ax.set_xlabel('Time /days')
+ax.set_ylabel('Number (Normalized by 1000)')
+ax.set_title('Baseline Model')
+ax.set_ylim(0,1.2)
+ax.yaxis.set_tick_params(length=0)
+ax.xaxis.set_tick_params(length=0)
+ax.grid(b=True, which='major', c='w', lw=2, ls='-')
+legend = ax.legend()
+legend.get_frame().set_alpha(0.5)
+for spine in ('top', 'right', 'bottom', 'left'):
+    ax.spines[spine].set_visible(False)
+plt.show()
