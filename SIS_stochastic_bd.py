b9f8232ea705a8b6aebd7083991f6f420ebf7557
Alex Lin <alexlin@Alexs-MacBook-Pro-53.local>
2018-03-03 22:39:51 -0500

    labels corrected in bd.py

--- SIS_stochastic_bd.py
+++ SIS_stochastic_bd.py
@@ -13,28 +13,24 @@ ND=30 * 24; #Time (a month)
 input = Y0
 
 # params related to birth and death
-v = 1;  
-p =
+p = 0.05; # probability of an informed agent being arrested
 
 def stochastic_equations(last_Y,ts):
 	Y=last_Y
-
-    #transmission rate = beta * XY/N
-	transmit_rate = beta*(N0-Y)*Y/N0
-    #recovery rate = gamma * Y
-	recover_rate = gamma*Y
+	denial_rate = beta*(N0-Y)*Y/(N0 * (1-p))
+	access_rate = gamma*Y
 
     #generate random numbers
 	rand1=pl.rand()
 	rand2=pl.rand()
 
     #time until either event occurs
-	ts = -np.log(rand2)/(transmit_rate+recover_rate)
-	if rand1 < (transmit_rate/(transmit_rate+recover_rate)):
-        # infection
+	ts = -np.log(rand2)/(denial_rate+access_rate)
+	if rand1 < (denial_rate/(denial_rate+access_rate)):
+        # denial, one more naive agent
 		Y += 1;
 	else:
-        # recovery
+        # access, one fewer naive agent
 		Y -= 1;
 	return [Y, ts]
 
@@ -43,31 +39,32 @@ def stochastic_iteration(input):
 	ts=0
     # Initialize as lists
 	T=[0]
-	infected=[0]
+	naive=[0]
 	while T[lop] < ND and input > 0:
 		[res,ts] = stochastic_equations(input,ts)
 		lop=lop+1
 		T.append(T[lop-1]+ts)
-		infected.append(input)
+		naive.append(input)
 		lop=lop+1
 		T.append(T[lop-1])
-		infected.append(res)
+		naive.append(res)
 		input=res
-	return [np.array(infected), np.array(T)]
+	return [np.array(naive), np.array(T)]
 
 # Visualize 4 simulations
-[infected1,t1]=stochastic_iteration(input)
-[infected2,t2]=stochastic_iteration(input)
-[infected3,t3]=stochastic_iteration(input)
-[infected4,t4]=stochastic_iteration(input)
+[naive1,t1]=stochastic_iteration(input)
+[naive2,t2]=stochastic_iteration(input)
+[naive3,t3]=stochastic_iteration(input)
+[naive4,t4]=stochastic_iteration(input)
+
 
 f1, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2, sharex='col', sharey='row');
-ax1.plot(t1/24., infected1);
+ax1.plot(t1/24., naive1);
 ax1.set_title('Simulation 1');
-ax2.plot(t2/24., infected2);
+ax2.plot(t2/24., naive2);
 ax2.set_title('Simulation 2');
-ax3.plot(t3/24., infected3);
+ax3.plot(t3/24., naive3);
 ax3.set_title('Simulation 3');
-ax4.plot(t4/24., infected4);
+ax4.plot(t4/24., naive4);
 ax4.set_title('Simulation 4');
 plt.show();
